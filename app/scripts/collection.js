7bc912eccd80a2250005f7176c31c5b09e9a1622
Richard <argaen21@gmail.com>
2014-10-06 17:01:53 +0000

    Added an overlay to the collection view

--- app/scripts/collection.js
+++ app/scripts/collection.js
@@ -1,7 +1,9 @@
  var buildAlbumThumbnail = function() {
     var template =
         '<div class="collection-album-container col-md-2">'
+      + '  <div class="collection-album-image-container">'
       + '  <img src="/images/album-placeholder.png"/>'
+      + '  </div>'
       + '  <div class="caption album-collection-info">'
       + '    <p>'
       + '      <a class="album-name" href="/album.html"> Album Name </a>'
@@ -20,16 +22,23 @@
  };
  
  var updateCollectionView = function() {
-   var $collection = $(".collection-container .row");
-   $collection.empty();
-   var albumLimit = Math.random() * 100;
-   if (albumLimit < 25){
-     albumLimit = 25;
-   }
-   for (var i = 0; i < albumLimit; i++) {
-     var $newThumbnail = buildAlbumThumbnail();
-     $collection.append($newThumbnail);
-   }
+       var $collection = $(".collection-container .row");
+  $collection.empty();
+
+  for (var i = 0; i < 33; i++) {
+    var $newThumbnail = buildAlbumThumbnail();
+    $collection.append($newThumbnail);
+  }
+
+  var onHover = function(event) {
+    $(this).append(buildAlbumOverlay("/album.html"));
+  };
+   
+  var offHover = function(event) {
+    $(this).find('.collection-album-image-overlay').remove();
+  };
+
+  $collection.find('.collection-album-image-container').hover(onHover, offHover);
  };
 
 if (document.URL.match(/\/collection.html/)) {
@@ -39,3 +48,20 @@ if (document.URL.match(/\/collection.html/)) {
     updateCollectionView();
   });
 }
+
+var buildAlbumOverlay = function(albumURL) {
+    var template =
+        '<div class="collection-album-image-overlay">'
+      + '  <div class="collection-overlay-content">'
+      + '    <a class="collection-overlay-button" href="' + albumURL + '">'
+      + '      <i class="fa fa-play"></i>'
+      + '    </a>'
+      + '    &nbsp;'
+      + '    <a class="collection-overlay-button">'
+      + '      <i class="fa fa-plus"></i>'
+      + '    </a>'
+      + '  </div>'
+      + '</div>'
+      ;
+    return $(template);
+  };
