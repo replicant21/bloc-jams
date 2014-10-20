b85f190042428db4b01843467263a15bdc8e59b5
Richard <argaen21@gmail.com>
2014-10-20 19:37:03 +0000

    Added functionality to player controls

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -227,6 +227,45 @@ var changeAlbumView = function (album) {
         $songList.append($newRow);
     }
 };
+
+ var setupSeekBars = function() {
+ 
+   $seekBars = $('.player-bar .seek-bar');
+   $seekBars.click(function(event) {
+     updateSeekPercentage($(this), event);
+   });
+   
+   $seekBars.find('.thumb').mousedown(function(event){
+    var $seekBar = $(this).parent();
+ 
+     $seekBar.addClass('no-animate');
+     
+    $(document).bind('mousemove.thumb', function(event){
+      updateSeekPercentage($seekBar, event);
+    });
+ 
+    //cleanup
+    $(document).bind('mouseup.thumb', function(){
+      $seekBar.removeClass('no-animate');
+      
+      $(document).unbind('mousemove.thumb');
+      $(document).unbind('mouseup.thumb');
+    });
+  });
+ };
+
+var updateSeekPercentage = function($seekBar, event) {
+   var barWidth = $seekBar.width();
+   var offsetX = event.pageX - $seekBar.offset().left; // get mouse x offset here
+ 
+   var offsetXPercent = (offsetX  / barWidth) * 100;
+   offsetXPercent = Math.max(0, offsetXPercent);
+   offsetXPercent = Math.min(100, offsetXPercent);
+ 
+   var percentageString = offsetXPercent + '%';
+   $seekBar.find('.fill').width(percentageString);
+   $seekBar.find('.thumb').css({left: percentageString});
+ }
 // This 'if' condition is used to prevent the jQuery modifications
 // from happening on non-Album view pages.
 // - Use a regex to validate that the url has "/album" in its path.
@@ -234,6 +273,7 @@ if (document.URL.match(/\/album.html/)) {
     // Wait until the HTML is fully processed.
     $(document).ready(function () {
         changeAlbumView(albumPicasso);
+      setupSeekBars();
     });
 }
 });
