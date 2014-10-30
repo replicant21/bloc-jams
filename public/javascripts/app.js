7f248c8f67a78d14efd0367db94784fe61941bb2
Richard <argaen21@gmail.com>
2014-10-30 17:14:43 +0000

    Built a SongPlayer service to share song data across views.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -349,11 +349,10 @@ blocJams.controller('Landing.controller', ['$scope', function($scope) {
    }
 }]);
 
-  blocJams.controller('Album.controller', ['$scope', function($scope) {
+  blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
    $scope.album = angular.copy(albumPicasso);
     
     var hoveredSong = null;
-   var playingSong = null;
  
    $scope.onHoverSong = function(song) {
      hoveredSong = song;
@@ -364,7 +363,7 @@ blocJams.controller('Landing.controller', ['$scope', function($scope) {
    };
     
     $scope.getSongState = function(song) {
-     if (song === playingSong) {
+     if (song === SongPlayer.currentSong && SongPlayer.playing) {
        return 'playing';
      }
      else if (song === hoveredSong) {
@@ -374,14 +373,37 @@ blocJams.controller('Landing.controller', ['$scope', function($scope) {
    };
     
     $scope.playSong = function(song) {
-      playingSong = song;
+      SongPlayer.setSong($scope.album, song);
+     SongPlayer.play();
     };
  
     $scope.pauseSong = function(song) {
-      playingSong = null;
+      SongPlayer.pause();
     };
  }]);
+   blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
+   $scope.songPlayer = SongPlayer;
+ }]);
   
+  blocJams.service('SongPlayer', function() {
+   return {
+     currentSong: null,
+     currentAlbum: null,
+     playing: false,
+ 
+     play: function() {
+       this.playing = true;
+     },
+     pause: function() {
+       this.playing = false;
+     },
+     setSong: function(album, song) {
+       this.currentAlbum = album;
+       this.currentSong = song;
+     }
+   };
+ });
+
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
